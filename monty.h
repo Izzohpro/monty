888c62392053eaeb347600181a41726b57c0392c
Izzohpro <okparafrankline@gmail.com>
2023-06-22 02:34:52 -0700

    monty

--- monty.h
+++ monty.h
@@ -16,4 +16,12 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-
+typedef struct global_s
+{
+        int is_lifo;
+        unsigned int line_count;
+        char *arg;
+        stack_t *stack;
+        FILE *file_descriptor;
+        char *buffer;
+} global_t;
