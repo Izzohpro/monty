8757c59b2ebc2959b01b4c1f6876c9e7c69566bd
raocindy <cindyisogie@gmail.com>
2023-06-22 16:06:47 -0700

    updated

--- monty.h
+++ monty.h
@@ -48,7 +48,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void execute_command(char *opcode, stack_t **stack, unsigned int line_number);
 void processFile(const char *filename);
 void _pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
--- opcodes1.c
+++ opcodes1.c
@@ -63,3 +63,28 @@ void _pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * _pop - removes the top element of the stack
+ * @stack: pointer to the head of stack.
+ * @line_number: line number
+ *
+ * Return:no return
+ */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	top = *stack;
+	*stack = (*stack)->next;
+
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+
+	free(top);
+}
